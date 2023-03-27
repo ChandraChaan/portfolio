0228a56eb8a69e327d08c6c3d6417c9c4b3f8706
Chandra <dchandrachaan@gmail.com>
2023-03-27 06:42:48 +0530

    name format changes

---
+++ lib/ui/responsive_ui.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+class ResponsiveHome extends StatelessWidget {
+  const ResponsiveHome({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // here add conditions to implement the responsive ui
+    return const Placeholder();
+  }
+}
