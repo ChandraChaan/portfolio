10e210276f76b78545b8f6b34d9b46adf2dda779
Chandra <dchandrachaan@gmail.com>
2023-04-13 09:33:12 +0530

    contacts page UI modifications

--- lib/contact/contact.dart
+++ lib/contact/contact.dart
@@ -6,101 +6,61 @@ import '../utils/font_style.dart';
 
 class Contact extends StatelessWidget {
   const Contact({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.grey[200],
       width: double.infinity,
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Column(
-              children: [
-                Row(
-                  children: [
-                    const Icon(
+      decoration: BoxDecoration(
+          color: Colors.grey[200],
+        border: const Border(
+          bottom: BorderSide(
+            color: Colors.black,
+            width: 50,
+          ),
+        ),
+      ),
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: const [
+                Expanded(
+                  flex: 1,
+                  child: ListTile(
+                    leading: Icon(
                       Icons.location_pin,
                       color: Colors.blueAccent,
-                      size: 60.0,
                     ),
-                    getTextStyle(
-                        "ADDRESS", FontWeight.bold, Colors.black, 30),
-                  ],
-                ),
-                SizedBox(
-                    height: (60),
-                    width: (190),
-                    child: Padding(
-                      padding: const EdgeInsets.only(left: 7.0),
-                      child: getTextStyle(
-                          "Kadapa, Andra Pradesh, India, 516227",
-                          FontWeight.normal,
-                          Colors.black45,
-                          18),
-                    )),
-              ],
-            ),
-            Column(
-              children: [
-                Row(
-                  children: [
-                    IconButton(
-                      iconSize: 45,
-                      color: Colors.blue,
-                      icon: const FaIcon(FontAwesomeIcons.phone),
-                      onPressed: () {},
-                    ),
-                    getTextStyle(
-                        "PHONE", FontWeight.bold, Colors.black, 30),
-                  ],
-                ),
-                SizedBox(
-                  height: (60),
-                  width: (160),
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 7.0),
-                    child: getTextStyle(
-                      "+91 7093 599 788",
-                      FontWeight.normal,
-                      Colors.black45,
-                      18,
-                    ),
+                    title: Text("Kadapa, Andra Pradesh, India, 516227"),
                   ),
                 ),
-              ],
-            ),
-            Column(
-              children: [
-                Row(
-                  children: [
-                    const Icon(
+                Expanded(
+                  flex: 1,
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: Colors.blueAccent,
+                    ),
+                    title: Text("+91 70 93 599 788"),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: ListTile(
+                    leading: Icon(
                       Icons.email_outlined,
                       color: Colors.blueAccent,
-                      size: 60.0,
                     ),
-                    getTextStyle(
-                        "EMAIL", FontWeight.bold, Colors.black, 30),
-                  ],
-                ),
-                SizedBox(
-                  height: (60),
-                  width: (160),
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 7.0),
-                    child: getTextStyle(
-                        "Email: dchandrachaan@gmail.com",
-                        FontWeight.normal,
-                        Colors.black45,
-                        18),
+                    title: Text("Email: dchandrachaan@gmail.com"),
                   ),
                 ),
               ],
             ),
-          ],
-        ),
+          ),
+          const SizedBox(height: 80,)
+        ],
       ),
     );
   }
