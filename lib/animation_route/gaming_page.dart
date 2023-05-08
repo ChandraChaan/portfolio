56a569aa023cb53af495f7df0fafa31d7cbd48f5
Aruna <darunaaru99@gmail.com>
2023-05-08 19:45:57 +0530

    gaming screen ui

--- lib/animation_route/gaming_page.dart
+++ lib/animation_route/gaming_page.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 class SecondPage extends StatefulWidget {
@@ -9,13 +8,57 @@ class SecondPage extends StatefulWidget {
 class _SecondPageState extends State<SecondPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Wellcome to timepass game'),
-      ),
-      body: const Center(
-        child: Text('This is the Timepass game'),
-      ),
-    );
+    // TODO: implement build
+    throw UnimplementedError();
   }
 }
+TextEditingController inputController = new TextEditingController();
+String _text = "";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Wellcome to timepass game'),
+        ),
+        body: Row(
+          children: [
+            // side menu
+            Expanded(
+              flex: 6,
+              child:
+              Column(
+                children: [
+                  TextField(
+                    controller: inputController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Enter a message',
+                    ),
+                  ),
+                  const SizedBox(height: 30),
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(){
+                        _text = inputController.text;
+                      }
+                    },
+                    child: const Text('Enabled'),
+                  ),
+    Text('$_text'),
+                ],
+              ),
+
+
+            ),
+
+            // body
+            Expanded(flex: 4,
+                child: Text('hii')),
+          ],
+        )
+        // const Center(
+        //   child: Text('This is the Timepass game'),
+        // ),
+        );
+  }
+
