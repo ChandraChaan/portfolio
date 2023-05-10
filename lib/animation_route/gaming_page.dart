b1806f753100825df600925dc015b8e08006df08
Aruna <darunaaru99@gmail.com>
2023-05-10 20:25:12 +0530

    experience screen ui changes

--- lib/animation_route/gaming_page.dart
+++ lib/animation_route/gaming_page.dart
@@ -1,64 +1,67 @@
 import 'package:flutter/material.dart';
 
-class SecondPage extends StatefulWidget {
+class Secondpage extends StatefulWidget {
+  const Secondpage({Key? key}) : super(key: key);
+
   @override
-  _SecondPageState createState() => _SecondPageState();
+  State<Secondpage> createState() => _SecondpageState();
 }
 
-class _SecondPageState extends State<SecondPage> {
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
-  }
-}
-TextEditingController inputController = new TextEditingController();
-String _text = "";
+class _SecondpageState extends State<Secondpage> {
+  TextEditingController inputController = new TextEditingController();
+  String _text = "";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Wellcome to timepass game'),
-        ),
-        body: Row(
-          children: [
-            // side menu
-            Expanded(
-              flex: 6,
-              child:
-              Column(
-                children: [
-                  TextField(
-                    controller: inputController,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Enter a message',
-                    ),
-                  ),
-                  const SizedBox(height: 30),
-                  ElevatedButton(
-                    onPressed: () {
-                      setState(){
-                        _text = inputController.text;
-                      }
-                    },
-                    child: const Text('Enabled'),
-                  ),
-    Text('$_text'),
-                ],
-              ),
+      appBar: AppBar(
+        title: const Text('Wellcome to timepass game'),
+      ),
+      body: Row(
+        children: [
+          // side menu
+          Expanded(
+            flex: 9,
+            child: Column(
+              children: [
+                SizedBox(height: 60,),
 
+                TextField(
+                  controller: inputController,
+                  decoration: InputDecoration(
+                    hintText: "enter the name",
+                    border: OutlineInputBorder(),
+                    // labelText: 'Enter a message',
+                  ),
+                ),
+                SizedBox(height: 20,),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      _text = inputController.text;
+                    });
+                  },
+                  child: Text(
+                    'click me',
+                    style:
+                        TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Text(
+                  '$_text',
+                  style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold),
+                ),
 
+                // const SizedBox(height: 30),
+              ],
             ),
+          ),
 
-            // body
-            Expanded(flex: 4,
-                child: Text('hii')),
-          ],
-        )
-        // const Center(
-        //   child: Text('This is the Timepass game'),
-        // ),
-        );
+          // body
+          Expanded(flex: 3, child: Text('hii')),
+        ],
+      ),
+    );
   }
 
+}
