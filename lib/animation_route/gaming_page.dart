405d41e621a7ad0ebd231936e1e85b9857b5d590
Aruna <darunaaru99@gmail.com>
2023-05-11 19:26:52 +0530

    whatsapp page ui

--- lib/animation_route/gaming_page.dart
+++ lib/animation_route/gaming_page.dart
@@ -66,79 +66,241 @@ import 'package:flutter/material.dart';
 //
 // }
 
-class WhatsAppChatUI extends StatefulWidget {
+// class WhatsAppChatUI extends StatefulWidget {
+//   @override
+//   _WhatsAppChatUIState createState() => _WhatsAppChatUIState();
+// }
+
+// class _WhatsAppChatUIState extends State<WhatsAppChatUI> {
+//   TextEditingController inputController = new TextEditingController();
+//   String _text = "";
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         titleSpacing: 0.0,
+//         leading: IconButton(
+//           icon: Icon(Icons.arrow_back),
+//           onPressed: () {
+//             Navigator.pop(context);
+//           },
+//         ),
+//         title: Row(
+//           children: [
+//             CircleAvatar(
+//               backgroundImage: NetworkImage(
+//                   'https://www.w3schools.com/howto/img_avatar.png'),
+//             ),
+//             SizedBox(
+//               width: 10.0,
+//             ),
+//             Text(
+//               'Jane Doe',
+//               style: TextStyle(fontSize: 18.0),
+//             ),
+//           ],
+//         ),
+//         actions: [
+//           IconButton(
+//             icon: Icon(Icons.call),
+//             onPressed: () {},
+//           ),
+//           IconButton(
+//             icon: Icon(Icons.videocam),
+//             onPressed: () {},
+//           ),
+//           IconButton(
+//             icon: Icon(Icons.more_vert),
+//             onPressed: () {},
+//           ),
+//         ],
+//       ),
+//       body: Column(
+//         children: [
+//           Expanded(
+//             child: ListView.builder(
+//               reverse: true,
+//               itemCount: 10,
+//               itemBuilder: (BuildContext context, int index) {
+//                 return ListTile(
+//                   leading: CircleAvatar(
+//                     backgroundImage: NetworkImage(
+//                         'https://www.w3schools.com/howto/img_avatar.png'),
+//                   ),
+//                   title: Text('Jane Doe'),
+//                   subtitle: Text('Hello, how are you?'),
+//                   trailing: Text('12:00 pm'),
+//                 );
+//               },
+//             ),
+//           ),
+//           Text(
+//             '$_text',
+//             style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold),
+//           ),
+//
+//           Divider(
+//             height: 1.0,
+//           ),
+//           Container(
+//             padding: EdgeInsets.symmetric(horizontal: 10.0),
+//             child: Row(
+//               children: [
+//                 Expanded(
+//                   child: TextField(
+//                     decoration: InputDecoration(
+//                       hintText: 'Type a message',
+//                       border: InputBorder.none,
+//                     ),
+//                   ),
+//                 ),
+//                 IconButton(
+//                   icon: Icon(Icons.attach_file),
+//                   onPressed: () {},
+//                 ),
+//                 IconButton(
+//                   icon: Icon(Icons.camera_alt),
+//                   onPressed: () {},
+//                 ),
+//                 IconButton(
+//                   icon: Icon(Icons.send),
+//                   onPressed: () {
+//                     setState(() {
+//                       _text = inputController.text;
+//                     });
+//                   },
+//                 ),
+//               ],
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
+
+class WhatsAppChat extends StatefulWidget {
   @override
-  _WhatsAppChatUIState createState() => _WhatsAppChatUIState();
+  State<WhatsAppChat> createState() => _WhatsAppChatState();
 }
 
-class _WhatsAppChatUIState extends State<WhatsAppChatUI> {
+class _WhatsAppChatState extends State<WhatsAppChat> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         titleSpacing: 0.0,
+        leadingWidth: 60.0,
         leading: IconButton(
-          icon: Icon(Icons.arrow_back),
-          onPressed: () {
-            Navigator.pop(context);
-          },
+          onPressed: () {},
+          icon: Icon(
+            Icons.arrow_back,
+            color: Colors.white,
+          ),
         ),
         title: Row(
           children: [
             CircleAvatar(
+              radius: 20.0,
               backgroundImage: NetworkImage(
-                  'https://www.w3schools.com/howto/img_avatar.png'),
+                  'https://randomuser.me/api/portraits/men/10.jpg'),
             ),
-            SizedBox(
-              width: 10.0,
-            ),
-            Text(
-              'Jane Doe',
-              style: TextStyle(fontSize: 18.0),
+            SizedBox(width: 10.0),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'John Doe',
+                  style: TextStyle(fontSize: 16.0),
+                ),
+                Text(
+                  'last seen today at 11:54 AM',
+                  style: TextStyle(fontSize: 12.0),
+                ),
+              ],
             ),
           ],
         ),
         actions: [
           IconButton(
-            icon: Icon(Icons.call),
             onPressed: () {},
+            icon: Icon(
+              Icons.call,
+              color: Colors.white,
+            ),
           ),
           IconButton(
-            icon: Icon(Icons.videocam),
             onPressed: () {},
+            icon: Icon(
+              Icons.videocam,
+              color: Colors.white,
+            ),
           ),
           IconButton(
-            icon: Icon(Icons.more_vert),
             onPressed: () {},
+            icon: Icon(
+              Icons.more_vert,
+              color: Colors.white,
+            ),
           ),
         ],
       ),
       body: Column(
         children: [
           Expanded(
-            child: ListView.builder(
-              reverse: true,
-              itemCount: 10,
-              itemBuilder: (BuildContext context, int index) {
-                return ListTile(
-                  leading: CircleAvatar(
-                    backgroundImage: NetworkImage(
-                        'https://www.w3schools.com/howto/img_avatar.png'),
-                  ),
-                  title: Text('Jane Doe'),
-                  subtitle: Text('Hello, how are you?'),
-                  trailing: Text('12:00 pm'),
-                );
-              },
+            child: ListView(
+              padding: EdgeInsets.symmetric(vertical: 10.0),
+              children: [
+                ChatMessage(
+                  message: 'Hey, how are you?',
+                  isMe: true,
+                  time: '11:50 AM',
+                ),
+                ChatMessage(
+                  message: 'I am doing good, thanks. What about you?',
+                  isMe: false,
+                  time: '11:52 AM',
+                ),
+                ChatMessage(
+                  message: 'I am fine too.',
+                  isMe: true,
+                  time: '11:54 AM',
+                ),
+                ChatMessage(
+                  message: 'What are your plans for today?',
+                  isMe: false,
+                  time: '11:56 AM',
+                ),
+                ChatMessage(
+                  message: 'Nothing much, just chilling at home. What about you?',
+                  isMe: true,
+                  time: '11:58 AM',
+                ),
+                ChatMessage(
+                  message: 'I have a meeting at 2 PM. Other than that, nothing planned.',
+                  isMe: false,
+                  time: '12:00 PM',
+                ),
+              ],
             ),
           ),
-          Divider(
-            height: 1.0,
-          ),
           Container(
-            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            padding: EdgeInsets.symmetric(vertical: 5.0),
+            decoration: BoxDecoration(
+              border: Border(
+                top: BorderSide(
+                  width: 1.0,
+                  color: Colors.grey,
+                ),
+              ),
+            ),
             child: Row(
               children: [
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.insert_emoticon),
+                ),
                 Expanded(
                   child: TextField(
                     decoration: InputDecoration(
@@ -148,16 +310,12 @@ class _WhatsAppChatUIState extends State<WhatsAppChatUI> {
                   ),
                 ),
                 IconButton(
+                  onPressed: () {},
                   icon: Icon(Icons.attach_file),
-                  onPressed: () {},
                 ),
                 IconButton(
+                  onPressed: () {},
                   icon: Icon(Icons.camera_alt),
-                  onPressed: () {},
-                ),
-                IconButton(
-                  icon: Icon(Icons.send),
-                  onPressed: () {},
                 ),
               ],
             ),
@@ -167,3 +325,21 @@ class _WhatsAppChatUIState extends State<WhatsAppChatUI> {
     );
   }
 }
+
+class ChatMessage extends StatelessWidget {
+  final String message;
+  final bool isMe;
+  final String time;
+
+  ChatMessage({required this.message, required this.isMe, required this.time});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+
+}
+
+
--- lib/animation_route/navigate_newpage.dart
+++ lib/animation_route/navigate_newpage.dart
@@ -3,7 +3,7 @@ import 'package:flutter/cupertino.dart';
 import 'gaming_page.dart';
 
 class SecondPageRoute extends CupertinoPageRoute {
-  SecondPageRoute() : super(builder: (BuildContext context) => WhatsAppChatUI());
+  SecondPageRoute() : super(builder: (BuildContext context) => WhatsAppChat());
 
   // OPTIONAL IF YOU WISH TO HAVE SOME EXTRA ANIMATION WHILE ROUTING
   @override
@@ -15,7 +15,7 @@ class SecondPageRoute extends CupertinoPageRoute {
           scale: animation,
           child: FadeTransition(
             opacity: animation,
-            child: WhatsAppChatUI(),
+            child: WhatsAppChat(),
           ),
         ));
   }
