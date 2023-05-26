7d6f2446dbf30c2cf92ef1eda7ad5ab3fcd6027f
Aruna <darunaaru99@gmail.com>
2023-05-27 00:00:20 +0530

    usersdata get api implimented

--- lib/animation_route/admin_data.dart
+++ lib/animation_route/admin_data.dart
@@ -1,13 +1,132 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-class AdminData extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+
+// class AdminData extends StatefulWidget {
+//   const AdminData({Key? key}) : super(key: key);
+//
+//   @override
+//   State<AdminData> createState() => _AdminDataState();
+// }
+//
+// class _AdminDataState extends State<AdminData> {
+//   void showNotificationPopup(BuildContext context, String recipientName) {
+//     showDialog(
+//       context: context,
+//       builder: (BuildContext context) {
+//         return const SendNotificationPopup(systemName: 'Mac', browserName: 'Chrome',);
+//       },
+//     );
+//   }
+//   @override
+//   void initState() {
+//     super.initState();
+//     callapi();
+//     print('Aruna1111');
+//
+//   }
+//
+//   List<dynamic> usersData = [];
+//
+//   Future<void> callAPI() async {
+//     final response = await http.get(Uri.parse('https://chandrachaan.in/randac/index.php/item/role_app_users'));
+//
+//     if (response.statusCode == 200) {
+//       Map<String, dynamic> jsonData = json.decode(response.body);
+//       usersData.addAll(jsonData["data"]);
+//
+//       setState(() {
+//         // Update your state variables here if needed
+//       });
+//
+//       print(usersData.length);
+//       print('Aruna');
+//
+//       // Do something with the response data
+//     } else {
+//       // Handle error
+//     }
+//   }
+//
+//
+//
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('User Data'),
+//       ),
+//
+//       body:usersData.length==0?Center(child: CircularProgressIndicator()): ListView.builder(
+//         itemCount: usersData.length,
+//         shrinkWrap: true,
+//         itemBuilder: (BuildContext context, int index) {
+//           return  UserDataScreen(
+//             address: usersData[index]['address'],
+//             systemName:  usersData[index]['system_name'],
+//             date: usersData[index]['date'],
+//             browserName: usersData[index]['browser_name'],
+//             onPre: (){
+//               showNotificationPopup(context, 'John Doe');
+//             },
+//
+//           );
+//         },
+//       ),
+//     );
+//   }
+//
+//   void callapi() {}
+// }
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class AdminData extends StatefulWidget {
   const AdminData({Key? key}) : super(key: key);
+
+  @override
+  State<AdminData> createState() => _AdminDataState();
+}
+
+class _AdminDataState extends State<AdminData> {
+  List<dynamic> usersData = [];
+
+  @override
+  void initState() {
+    super.initState();
+    callAPI();
+    print('Aruna1111');
+  }
+
+  Future<void> callAPI() async {
+    final response = await http.get(Uri.parse('https://chandrachaan.in/randac/index.php/item/role_app_users'));
+
+    if (response.statusCode == 200) {
+      Map<String, dynamic> jsonData = json.decode(response.body);
+      usersData.addAll(jsonData["data"]);
+
+      setState(() {
+        // Update your state variables here if needed
+      });
+
+      print(usersData.length);
+      print('Aruna');
+
+      // Do something with the response data
+    } else {
+      // Handle error
+    }
+  }
 
   void showNotificationPopup(BuildContext context, String recipientName) {
     showDialog(
       context: context,
       builder: (BuildContext context) {
-        return const SendNotificationPopup(systemName: 'Mac', browserName: 'Chrome',);
+        return const SendNotificationPopup(systemName: 'Mac', browserName: 'Chrome');
       },
     );
   }
@@ -18,25 +137,93 @@ class AdminData extends StatelessWidget {
       appBar: AppBar(
         title: const Text('User Data'),
       ),
-      body: ListView.builder(
-        itemCount: 7,
+      body: usersData.length == 0
+          ? Center(child: CircularProgressIndicator())
+          : ListView.builder(
+        itemCount: usersData.length,
         shrinkWrap: true,
         itemBuilder: (BuildContext context, int index) {
-          return  UserDataScreen(
-            address: '123 Main St',
-            systemName: 'Flutter App',
-            date: 'May 25, 2023',
-            browserName: 'Chrome',
-            onPre: (){
+          return UserDataScreen(
+            address: usersData[index]['address'],
+            systemName: usersData[index]['system_name'],
+            date: usersData[index]['date'],
+            browserName: usersData[index]['browser_name'],
+            onPre: () {
               showNotificationPopup(context, 'John Doe');
             },
-
           );
         },
       ),
     );
   }
 }
+
+// class UserDataScreen extends StatelessWidget {
+//   final String address;
+//   final String systemName;
+//   final String date;
+//   final String browserName;
+//   final VoidCallback onPre;
+//
+//   const UserDataScreen({
+//     Key? key,
+//     required this.address,
+//     required this.systemName,
+//     required this.date,
+//     required this.browserName,
+//     required this.onPre,
+//   }) : super(key: key);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return ListTile(
+//       title: Text(address),
+//       subtitle: Column(
+//         crossAxisAlignment: CrossAxisAlignment.start,
+//         children: [
+//           Text(systemName),
+//           Text(date),
+//           Text(browserName),
+//         ],
+//       ),
+//       onTap: onPre,
+//     );
+//   }
+// }
+
+// class SendNotificationPopup extends StatelessWidget {
+//   final String systemName;
+//   final String browserName;
+//
+//   const SendNotificationPopup({
+//     Key? key,
+//     required this.systemName,
+//     required this.browserName,
+//   }) : super(key: key);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return AlertDialog(
+//       title: const Text('Send Notification'),
+//       content: Column(
+//         crossAxisAlignment: CrossAxisAlignment.start,
+//         mainAxisSize: MainAxisSize.min,
+//         children: [
+//           Text('System Name: $systemName'),
+//           Text('Browser Name: $browserName'),
+//         ],
+//       ),
+//       actions: [
+//         TextButton(
+//           onPressed: () {
+//             Navigator.of(context).pop();
+//           },
+//           child: const Text('Close'),
+//         ),
+//       ],
+//     );
+//   }
+// }
 
 class UserDataScreen extends StatelessWidget {
   final String address;
