9bb2e5f07506106e13d0aa2636398382ff24d353
Chandra <dchandrachaan@gmail.com>
2023-06-12 09:08:47 +0530

    player and images changes done

--- lib/utils/dynamic_image.dart
+++ lib/utils/dynamic_image.dart
@@ -1,29 +1,61 @@
-// import 'package:audioplayers/audioplayers.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
-import '../providers/user_info.dart';
-
-class ImageDynamic extends StatelessWidget {
+class ImageDynamic extends StatefulWidget {
   final String img;
 
-  const ImageDynamic({super.key, required this.img});
+  const ImageDynamic({Key? key, required this.img}) : super(key: key);
+
+  @override
+  _ImageDynamicState createState() => _ImageDynamicState();
+}
+
+class _ImageDynamicState extends State<ImageDynamic> {
+  bool _loadingFailed = false;
 
   @override
   Widget build(BuildContext context) {
-    return img.contains('assets')
+    return widget.img.contains('assets')
         ? Image.asset(
-            img,
+            widget.img,
             fit: BoxFit.fitHeight,
           )
-        : CachedNetworkImage(
-            imageUrl: img,
-            placeholder: (context, url) =>
-                const Center(child: CircularProgressIndicator()),
-            errorWidget: (context, url, error) =>
-                const Icon(Icons.image_not_supported),
-            fit: BoxFit.cover,
-          );
+        : _loadingFailed
+            ? Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const Text('Failed to load image.', textAlign:TextAlign.center),
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        _loadingFailed = false;
+                      });
+                    },
+                    child: const Text('Reload'),
+                  ),
+                ],
+              )
+            : CachedNetworkImage(
+                imageUrl: widget.img,
+                placeholder: (context, url) =>
+                    const Center(child: CircularProgressIndicator()),
+                errorWidget: (context, url, error) {
+                  WidgetsBinding.instance.addPostFrameCallback((_) {
+                    setState(() {
+                      _loadingFailed = true;
+                    });
+                  });
+                  return const Icon(Icons.image_not_supported);
+                },
+                fit: BoxFit.cover,
+                imageBuilder: (context, imageProvider) {
+                  return Image(
+                    image: imageProvider,
+                    fit: BoxFit.cover,
+                  );
+                },
+                httpHeaders: const {},
+              );
   }
 }
