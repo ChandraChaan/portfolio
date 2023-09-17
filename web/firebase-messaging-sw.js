9ca89335cc48448091a48793a11d34062ec36e32
Chandra <dchandrachaan@gmail.com>
2023-09-18 01:42:23 +0530

    service worker changes done

--- web/firebase-messaging-sw.js
+++ web/firebase-messaging-sw.js
@@ -1,24 +1,22 @@
 importScripts('https://www.gstatic.com/firebasejs/8.4.1/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/8.4.1/firebase-messaging.js');
 
-/* Update with your config */
-    const firebaseConfig = {
-      apiKey: "AIzaSyCcyArg2tLT6FgAIvfe_mU2Q1DAzGC1gD4",
-      authDomain: "rollapp123-6b732.firebaseapp.com",
-      projectId: "rollapp123-6b732",
-      storageBucket: "rollapp123-6b732.appspot.com",
-      messagingSenderId: "873492171667",
-      appId: "1:873492171667:web:f210c8d97d4b34f169b666",
-      measurementId: "G-9X1ZNVD6ZY"
-    };
+const firebaseConfig = {
+  apiKey: "AIzaSyCcyArg2tLT6FgAIvfe_mU2Q1DAzGC1gD4",
+  authDomain: "rollapp123-6b732.firebaseapp.com",
+  projectId: "rollapp123-6b732",
+  storageBucket: "rollapp123-6b732.appspot.com",
+  messagingSenderId: "873492171667",
+  appId: "1:873492171667:web:f210c8d97d4b34f169b666",
+  measurementId: "G-9X1ZNVD6ZY"
+};
 
 firebase.initializeApp(firebaseConfig);
+
 const messaging = firebase.messaging();
 
-// Handle background messages
-messaging.onBackgroundMessage(function(payload) {
+messaging.onBackgroundMessage((payload) => {
   console.log('Received background message ', payload);
-
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
@@ -27,4 +25,3 @@ messaging.onBackgroundMessage(function(payload) {
 
   self.registration.showNotification(notificationTitle, notificationOptions);
 });
-
